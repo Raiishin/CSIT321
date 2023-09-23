7dfacefa15259fbb57368adb9eb896e0993a9934
raiishin <gavin.tanjy11@gmail.com>
2023-09-23 08:36:29 +0800

    Added :: Temporarily implement edit existing user page

--- app/src/App.js
+++ app/src/App.js
@@ -12,6 +12,7 @@ import Attendance from './components/Attendance';
 import Timetable from './components/Timetable';
 import CreateAccountPage from './components/CreateAccountPage';
 import EditAccountPage from './components/EditAccountPage';
+import EditExistingUserPage from './components/EditExistingUserPage';
 
 import useGlobalStore from './store/globalStore';
 import { isUndefined } from 'lodash';
@@ -34,8 +35,9 @@ const App = () => {
           <Route path="/timetable" element={<Timetable />} />
           <Route path="/attendance" element={<Attendance />} />
 
+          <Route path="/account/" element={<EditAccountPage />} />
           <Route path="/account/create" element={<CreateAccountPage />} />
-          <Route path="/account/edit" element={<EditAccountPage />} />
+          <Route path="/account/edit" element={<EditExistingUserPage />} />
         </Routes>
       </div>
     </div>
--- app/src/components/EditExistingUserPage.jsx
+++ app/src/components/EditExistingUserPage.jsx
@@ -1,52 +1,64 @@
-// Body.jsx
 import React from 'react';
-import './style.css'; // Import the corresponding CSS file
-import timetablebtn from '../assets/timetablebtn.jpg';
-import takeattendancebtn from '../assets/takeattendancebtn.jpg';
-import logoutbtn from '../assets/logoutbtn.jpg';
-import Editingbtn from '../assets/Editingbtn.png';
 
-function EditExistingUserPage() {
+import editIcon from '../assets/editing.png';
+
+const EditExistingUserPage = () => {
   return (
-    <div className='fixed grid grid-cols-3 pt-9 w-full'>
-        <div class='col-span-3 flex justify-center pt-16 bg-zinc-900'>
-        <p className='text-[#ccd6f6] text-4xl font-bold'>Edit Existing Account</p></div>
+    <div className="grid grid-cols-3 w-full">
+      <div class="col-span-3 flex justify-center ">
+        <p className="text-[#ccd6f6] text-4xl font-bold">Edit Existing Account</p>
+      </div>
 
-        <div className='w-[50%] col-span-3 fixed max-h-[60%] mt-32 ml-96'>
-        <table className='border-collapse bg-[#b5b5b6] border-b-2 border-slate-500 w-[100%] font-sans'>
-        <tbody>
-          <tr class='bg-white'>
-            <td class='bg-[#cbcbcb] p-3 text-sm font-semibold tracking-wide text-left'>Name:</td>
-            <td class='p-3 text-sm font-semibold tracking-wide text-left'>John Doe</td>
-            <td class='p-3 text-sm font-semibold tracking-wide text-left'>
-                <button><img src={Editingbtn} alt="Editingbtn" class='w-6'/></button></td>
-          </tr>
-          <tr class='bg-white'>
-            <td class='bg-[#cbcbcb] p-3 text-sm font-semibold tracking-wide text-left'>Address:</td>
-            <td class='p-3 text-sm font-semibold tracking-wide text-left'>Block 123 Maven Road 4 Singapore 654321</td>
-            <td class='p-3 text-sm font-semibold tracking-wide text-left'>
-                <button><img src={Editingbtn} alt="Editingbtn" class='w-6'/></button></td>
-          </tr>
-          <tr class='bg-white'>
-            <td class='bg-[#cbcbcb] p-3 text-sm font-semibold tracking-wide text-left'>Email:</td>
-            <td class='p-3 text-sm font-semibold tracking-wide text-left'>john_doe123@mymail.sim.edu.sg</td>
-            <td class='p-3 text-sm font-semibold tracking-wide text-left'>
-                <button><img src={Editingbtn} alt="Editingbtn" class='w-6'/></button></td>
-          </tr>
-          <tr class='bg-white'>
-            <td class='bg-[#cbcbcb] p-3 text-sm font-semibold tracking-wide text-left'>Type:</td>
-            <td class='p-3 text-sm font-semibold tracking-wide text-left'>Student</td>
-            <td class='p-3 text-sm font-semibold tracking-wide text-left'>
-                <button><img src={Editingbtn} alt="Editingbtn" class='w-6'/></button></td>
-          </tr>
-        </tbody>
-      </table>
-
-        </div>
-
-        </div>
-      
+      <div className="w-[50%] col-span-3 max-h-[60%] mt-32 ml-96">
+        <table className="border-collapse bg-[#b5b5b6] border-b-2 border-slate-500 w-[100%] font-sans">
+          <tbody>
+            <tr class="bg-white">
+              <td class="bg-[#cbcbcb] p-3 text-sm font-semibold tracking-wide text-left">Name:</td>
+              <td class="p-3 text-sm font-semibold tracking-wide text-left">John Doe</td>
+              <td class="p-3 text-sm font-semibold tracking-wide text-left">
+                <button>
+                  <img src={editIcon} alt="editIcon" class="w-6" />
+                </button>
+              </td>
+            </tr>
+            <tr class="bg-white">
+              <td class="bg-[#cbcbcb] p-3 text-sm font-semibold tracking-wide text-left">
+                Address:
+              </td>
+              <td class="p-3 text-sm font-semibold tracking-wide text-left">
+                Block 123 Maven Road 4 Singapore 654321
+              </td>
+              <td class="p-3 text-sm font-semibold tracking-wide text-left">
+                <button>
+                  <img src={editIcon} alt="editIcon" class="w-6" />
+                </button>
+              </td>
+            </tr>
+            <tr class="bg-white">
+              <td class="bg-[#cbcbcb] p-3 text-sm font-semibold tracking-wide text-left">Email:</td>
+              <td class="p-3 text-sm font-semibold tracking-wide text-left">
+                john_doe123@mymail.sim.edu.sg
+              </td>
+              <td class="p-3 text-sm font-semibold tracking-wide text-left">
+                <button>
+                  <img src={editIcon} alt="editIcon" class="w-6" />
+                </button>
+              </td>
+            </tr>
+            <tr class="bg-white">
+              <td class="bg-[#cbcbcb] p-3 text-sm font-semibold tracking-wide text-left">Type:</td>
+              <td class="p-3 text-sm font-semibold tracking-wide text-left">Student</td>
+              <td class="p-3 text-sm font-semibold tracking-wide text-left">
+                <button>
+                  <img src={editIcon} alt="editIcon" class="w-6" />
+                </button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
   );
-}
+};
 
 export default EditExistingUserPage;
