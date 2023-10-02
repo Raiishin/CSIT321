054e20e78871ded118ab9cd5affe9ed3c6a8c959
Joe Tian <joetian@Joes-MacBook-Pro.local>
2023-10-02 23:46:25 +0800

    Added :: Login Page

--- app/src/components/Login.jsx
+++ app/src/components/Login.jsx
@@ -11,14 +11,54 @@ const Login = () => {
   console.log('userId', userId);
 
   return (
-    <div>
-      <form class="w-full max-w-sm">
+    <div className="pt-20">
+      <div className="max-w-sm mx-auto py-10 p-6 rounded shadow-md bg-[#F0F8FF]">
+        <h2 className="text-2xl font-semibold mb-4 text-center	">Sign In</h2>
+        <form>
+          <div className="mb-4">
+            <label htmlFor="email" className="block text-gray-700 font-bold mb-2">
+              Email
+            </label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              required
+              className="w-full px-3 py-2 border rounded-lg focus:outline-none focus:border-blue-500 shadow-md"
+            />
+          </div>
+          <div className="mb-4">
+            <label htmlFor="password" className="block text-gray-700 font-bold mb-2">
+              Password
+            </label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              required
+              className="w-full px-3 py-2 border rounded-lg focus:outline-none focus:border-blue-500 shadow-md"
+            />
+          </div>
+          <div className="mb-4 pt-5">
+            <button
+              type="submit"
+              className="w-full bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+              Login
+            </button>
+          </div>
+          <div className="text-start">
+            <a href="/reset-password" className="text-gray-500 hover:underline">
+              Forgot Password? Click here
+            </a>
+          </div>
+        </form>
+      </div>
+      {/* <form class="w-full max-w-sm">
         <div class="md:flex md:items-center mb-6">
           <div class="md:w-1/3">
             <label
               class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
-              for="inline-full-name"
-            >
+              for="inline-full-name">
               Email:
             </label>
           </div>
@@ -27,16 +67,14 @@ const Login = () => {
               class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
               id="inline-full-name"
               type="text"
-              value="email"
-            ></input>
+              value="email"></input>
           </div>
         </div>
         <div class="md:flex md:items-center mb-6">
           <div class="md:w-1/3">
             <label
               class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
-              for="inline-password"
-            >
+              for="inline-password">
               Password
             </label>
           </div>
@@ -45,11 +83,10 @@ const Login = () => {
               class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
               id="inline-password"
               type="password"
-              placeholder="******************"
-            ></input>
+              placeholder="******************"></input>
           </div>
         </div>
-      </form>
+      </form> */}
     </div>
   );
 };
--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  safelist: ['bg-red-500', 'text-3xl', 'lg:text-4xl'],
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {
@@ -7,16 +8,21 @@ module.exports = {
         purple: '#343a5e',
         blue: '#1E40AF',
         'light-blue': '#3B82F6',
+        'blue-500': 'rgb(59 130 246)',
+        'blue-700': 'rgb(29 78 216)',
         brown: '#AC8B54',
         'light-brown': '#4C453C',
         'dark-brown': '#272320',
         'light-gray': '#d1d5db',
         'dark-gray': '#4b5563',
         red: '#ef4444',
-		'light-cyan':"#9CFFE7",
+        'light-cyan': '#9CFFE7',
         cyan: '#ccd6f6',
         'dark-cyan': '#0e7490',
-		gold:"#D4B238"
+        gold: '#D4B238',
+        boxShadow: {
+          '5xl': '1 35px 60px -15px rgba(50, 50, 50, 0.3)'
+        }
       }
     }
   },
