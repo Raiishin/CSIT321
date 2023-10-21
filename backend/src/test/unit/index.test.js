134a3e8662258fd7b95206ad2584f304906c5ed5
&D <61130009+andykschan94@users.noreply.github.com>
2023-10-21 21:11:33 +0800

    Refactor:Index Unit Testing
    - refactor index unit testing for convertTimeStringToDate to UTC timezone

--- backend/src/test/unit/index.test.js
+++ backend/src/test/unit/index.test.js
@@ -2,7 +2,7 @@ import userTypeEnum from '../../constants/userTypeEnum.js';
 import { convertTimeStringToDate, getObjectKey } from '../../library/index.js';
 
 test('Convert Time String to Date', async () => {
-
+  
   //Invalid Date String
   expect(convertTimeStringToDate('2023qwe9', '50:00')).toMatchInlineSnapshot(`Date { NaN }`);
 
@@ -13,12 +13,13 @@ test('Convert Time String to Date', async () => {
   expect(convertTimeStringToDate('2023-01-11', '19sasad')).toMatchInlineSnapshot(`Date { NaN }`);
 
   //Valid Date and Time String
-  const validResult1 = convertTimeStringToDate('2023-03-21', '08:00');
-  expect(validResult1.toISOString()).toMatchInlineSnapshot(`"2023-03-21T00:00:00.000Z"`);
+  expect(convertTimeStringToDate('2023-03-21', '08:00')).toMatchInlineSnapshot(`2023-03-21T00:00:00.000Z`);
 
   //Valid Date and Time String
-  const validResult2 = convertTimeStringToDate('2023-01-50', '15-00');
-  expect(validResult2.toISOString()).toMatchInlineSnapshot(`"2023-02-19T07:00:00.000Z"`);
+  expect(convertTimeStringToDate('2023-01-50', '15:00')).toMatchInlineSnapshot(`2023-02-19T07:00:00.000Z`);
+  
+  //Valid Date and Time String
+  expect(convertTimeStringToDate('2023-12-30', '11:00')).toMatchInlineSnapshot(`2023-12-30T03:00:00.000Z`);
 
 });
 
